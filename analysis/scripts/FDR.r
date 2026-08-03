f0f3f6585a60a16ccc1673612ca3c369662768d2
Sajanth <sajanthk5@gmail.com>
2026-08-03 16:28:01 -0400

    Refactor FDR script to accept results file as argument

--- analysis/scripts/FDR.r
+++ analysis/scripts/FDR.r
@@ -1,13 +1,11 @@
 #!/usr/bin/env Rscript
-
-library(stats)
 library(tidyr)
 
 args <- commandArgs(trailingOnly=TRUE)
-cohort <- args[1]
+results_file <- args[1]
 
 # Load your data files
-all_genes <- read.table(paste0("SKAT/", cohort, ".results.skato"), sep = " ", header = TRUE)
+all_genes <- read.table(results_file, sep =" ", header = TRUE)
 
 #Reformat table to have _type as 2nd column; can modify to pivot table where all p values are columns in same row (=gene)
 extract_components <- function(data) {
@@ -24,4 +22,4 @@ all_genes$adjusted_P.value <- p.adjust(all_genes$P.value, method = "fdr")
 # Reorder columns to place adjusted_P.value in the fourth position
 all_genes <- all_genes[, c(1:4, ncol(all_genes), 5:(ncol(all_genes) - 1))]
 #print(all_genes)
-write.table(all_genes, paste0("annotation/", cohort, ".results.skato.adjusted.txt"), sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(all_genes, paste0(results_file, "_adjusted.txt"), sep = "\t", row.names = FALSE, quote = FALSE)
